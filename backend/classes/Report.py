4c7ce866cf522dde5038546b6545fa162d602bd9
Pedro Santini <pedroantoniosantini@gmail.com>
2024-06-22 15:17:35 -0300

    feat: create report and address classes. Fixed Sample and Property

---
+++ backend/classes/Report.py
@@ -0,0 +1,3 @@
+class Report:
+    def __init__(self, file_location: str) -> None:
+        self.file_location: str = file_location
--- backend/classes/Sample.py
+++ backend/classes/Sample.py
@@ -1,25 +1,24 @@
 class Sample:
     def __init__(self, name_description: str, total_area: float, collection_depth: float, phosphorus: float, potassium: float, organic_matter: float, pH: float, SMP: float, aluminum: float, H_Al: float, calcium: float, magnesium: float, copper: float, iron: float, manganese: float, zinc: float, base_saturation: float, CEC: float, v: float, aluminum_saturation: float, effective_CEC: float) -> None:
-        self.name_description = name_description
-        self.total_area = total_area
-        self.collection_depth = collection_depth
-        self.phosphorus = phosphorus
-        self.potassium = potassium
-        self.organic_matter = organic_matter
-        self.pH = pH
-        self.SMP = SMP
-        self.aluminum = aluminum
-        self.H_Al = H_Al
-        self.calcium = calcium
-        self.magnesium = magnesium
-        self.copper = copper
-        self.iron = iron
-        self.manganese = manganese
-        self.zinc = zinc
-        self.base_saturation = base_saturation
-        self.CEC = CEC
-        self.v = v
-        self.aluminum_saturation = aluminum_saturation
-        self.effective_CEC = effective_CEC
+        self.name_description: str = name_description
+        self.total_area: float = total_area
+        self.collection_depth: float = collection_depth
+        self.phosphorus: float = phosphorus
+        self.potassium: float = potassium
+        self.organic_matter: float = organic_matter
+        self.pH: float = pH
+        self.SMP: float = SMP
+        self.aluminum: float = aluminum
+        self.H_Al: float = H_Al
+        self.calcium: float = calcium
+        self.magnesium: float = magnesium
+        self.copper: float = copper
+        self.iron: float = iron
+        self.manganese: float = manganese
+        self.zinc: float = zinc
+        self.base_saturation: float = base_saturation
+        self.CEC: float = CEC
+        self.v: float = v
+        self.aluminum_saturation: float = aluminum_saturation
+        self.effective_CEC: float = effective_CEC
 
-#ainda sem saber bem certo os tipos das variáveis
