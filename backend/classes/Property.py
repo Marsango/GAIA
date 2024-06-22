89173ad4a46af73dbb1a63da5eb39fbdff0fc8c9
Pedro Santini <pedroantoniosantini@gmail.com>
2024-06-22 02:56:50 -0300

    FIRST COMMIT

---
+++ backend/classes/Property.py
@@ -0,0 +1,6 @@
+class Property:
+    def __init__(self, latitude: float, longitude: float, name: str, location: str) -> None:
+        self.latitude = latitude
+        self.longitude = longitude
+        self.name = name
+        self.location = location
---
+++ backend/classes/Sample.py
@@ -0,0 +1,25 @@
+class Sample:
+    def __init__(self, name_description: str, total_area: float, collection_depth: float, phosphorus: float, potassium: float, organic_matter: float, pH: float, SMP: float, aluminum: float, H_Al: float, calcium: float, magnesium: float, copper: float, iron: float, manganese: float, zinc: float, base_saturation: float, CEC: float, v: float, aluminum_saturation: float, effective_CEC: float) -> None:
+        self.name_description = name_description
+        self.total_area = total_area
+        self.collection_depth = collection_depth
+        self.phosphorus = phosphorus
+        self.potassium = potassium
+        self.organic_matter = organic_matter
+        self.pH = pH
+        self.SMP = SMP
+        self.aluminum = aluminum
+        self.H_Al = H_Al
+        self.calcium = calcium
+        self.magnesium = magnesium
+        self.copper = copper
+        self.iron = iron
+        self.manganese = manganese
+        self.zinc = zinc
+        self.base_saturation = base_saturation
+        self.CEC = CEC
+        self.v = v
+        self.aluminum_saturation = aluminum_saturation
+        self.effective_CEC = effective_CEC
+
+#ainda sem saber bem certo os tipos das variáveis
